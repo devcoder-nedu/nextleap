716939129e9724bbe01652dac0a6c66dd1823325
Chinedu Mgbemena <mgbemena.nedu@gmail.com>
2025-05-15 15:04:04 +0100

    ignored eslint and typescript checks during builds

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import {StartupTypeCard} from "@/components/StartupCard";
 import StartupCard from "@/components/StartupCard";
 import {STARTUPS_QUERY} from "@/sanity/lib/queries";
 import {sanityFetch, SanityLive} from "@/sanity/lib/live";
-import {auth} from "@/auth";
+// import {auth} from "@/auth";
 
 export default async function Home({searchParams}:
                                    {
@@ -13,7 +13,7 @@ export default async function Home({searchParams}:
     const query = (await searchParams).query;
     const params = {search: query || null};
 
-    const session = await auth();
+    // const session = await auth();
 
 
     const {data: posts} = await sanityFetch({query: STARTUPS_QUERY, params});
--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,28 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
     images: {
         dangerouslyAllowSVG: true,
         remotePatterns: [
             {
                 protocol: "https",
-                hostname:"*"
+                hostname: "*"
             }
         ]
     },
     // experimental:{
     //     ppr:"incremental",
     // },
-    devIndicators:{
+    devIndicators: {
         position: "bottom-right",
     }
-  /* config options here */
+    /* config options here */
 };
 
 export default nextConfig;
