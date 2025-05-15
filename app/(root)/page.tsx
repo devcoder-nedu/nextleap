d915eda2755152cdec4560a12d4091782a020452
Chinedu Mgbemena <mgbemena.nedu@gmail.com>
2025-05-15 16:41:18 +0100

    minor edits

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -14,6 +14,9 @@ export default async function Home({searchParams}:
     const query = (await searchParams).query;
     const params = {search: query || null};
 
+    console.log("query param:", query);
+
+
     const session = await auth();
     console.log(session?.id)
 
@@ -21,6 +24,9 @@ export default async function Home({searchParams}:
     const {data: posts} = await sanityFetch({query: STARTUPS_QUERY, params});
 
     console.log(posts);
+    console.log("query param:", query);
+    console.log("posts fetched:", posts.length);
+    console.log("First post:", posts?.[0]);
 
     return (
         <div>
@@ -38,17 +44,15 @@ export default async function Home({searchParams}:
                 </p>
 
                 <ul className={"mt-7 card_grid"}>
-                    <Suspense fallback={<StartupCardSkeleton/>}>
-                        {posts?.length > 0 ? (
-                                posts.map((post: StartupTypeCard) => (
-                                    <StartupCard key={post?._id} post={post}/>
-                                ))
-                            ) :
-                            (
-                                <p className={"no-result"}> No startups found </p>
-                            )
-                        }
-                    </Suspense>
+                    {posts?.length > 0 ? (
+                            posts.map((post: StartupTypeCard) => (
+                                <StartupCard key={post?._id} post={post}/>
+                            ))
+                        ) :
+                        (
+                            <p className={"no-result"}> No startups found </p>
+                        )
+                    }
 
                 </ul>
             </section>
